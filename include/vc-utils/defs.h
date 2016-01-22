a1288114fad06cacfa76b1982cf0a91f761ef2cb
Christopher Leonard <chris.j.leonard@gmx.com>
2016-01-22 14:43:11 +0100

    Added HashFn

--- include/vc-utils/defs.h
+++ include/vc-utils/defs.h
@@ -12,9 +12,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stddef.h>
 #include <stdbool.h>
 
-// An EqualFn will check to see if two given objects are considered the same
-typedef bool (*EqualFn)(const void *, const void *);
-
 // An InitFn will initialise an object at the first pointer, as a copy of the
 // object at the second pointer, and returns the first pointer.
 typedef void *(*InitFn)(void *restrict, const void *restrict);
@@ -22,5 +19,12 @@ typedef void *(*InitFn)(void *restrict, const void *restrict);
 // A DestFn will destroy an object at the pointer, freeing any resources
 // allocated during its life or in a Map_Init.
 typedef void (*DestFn)(void *);
+
+// An EqualFn will check to see if two given objects are considered the same
+typedef bool (*EqualFn)(const void *, const void *);
+
+// A HashFn takes a pointer to the object, and generates a deterministic
+// value based on it. Ideally the hash has good spread, and in constant time.
+typedef size_t (*HashFn)(const void *);
 
 #endif // VC_DEFS_H_INCLUDED
