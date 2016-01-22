cbb7f37ab12389920a950319b3910d91ef1168b3
Christopher Leonard <chris.j.leonard@gmx.com>
2016-01-22 01:42:09 +0100

    Added license note to file

--- test/List.c
+++ test/List.c
@@ -1,3 +1,11 @@
+/*
+vc-utils  General C utilities
+Copyright (C) 2016  Christopher Leonard
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
